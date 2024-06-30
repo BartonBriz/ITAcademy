423e9374aa2d2639e2161992d61902a2c35ae02a
BrizGLy <Brizgly@mail.ru>
2024-06-30 19:00:24 +0300

    HomeWork2

---
+++ module2/HomeWorkFactorial/HomeWorkFactorial/HeaderFactorial.h
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int a;
+    int fact = 1;
+    cin >> a;
+    if (a < 0)
+    {
+        cout << " Факториал не может быть меньше 0!" << endl;
+        return 0;
+    }
+    else if (a == 0)
+    {
+        cout << "Факториал нуля равен 1.";
+    }
+    else if (a > 1)
+    {
+        for (int i = 1; i <= a; i++)
+        {
+            fact = fact * i;
+
+        }
+        cout << "Факториал равен: " << fact << endl;
+    }
+}
