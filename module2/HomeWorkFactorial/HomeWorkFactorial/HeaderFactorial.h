3696c9e2e822610dccbf60bff537ca66e6be1258
BrizGLy <Brizgly@mail.ru>
2024-06-30 19:42:07 +0300

    Ubran russkiy yazik

--- module2/HomeWorkFactorial/HomeWorkFactorial/HeaderFactorial.h
+++ module2/HomeWorkFactorial/HomeWorkFactorial/HeaderFactorial.h
@@ -4,18 +4,19 @@ using namespace std;
 
 int main()
 {
-    setlocale(LC_ALL, "Russian");
+    
     int a;
     int fact = 1;
+    cout << "Vvedite chislo: ";
     cin >> a;
     if (a < 0)
     {
-        cout << " Факториал не может быть меньше 0!" << endl;
+        cout << " Factorial ne mojet bit' men'she 0!" << endl;
         return 0;
     }
     else if (a == 0)
     {
-        cout << "Факториал нуля равен 1.";
+        cout << "Factorial null raven 1.";
     }
     else if (a > 1)
     {
@@ -24,6 +25,6 @@ int main()
             fact = fact * i;
 
         }
-        cout << "Факториал равен: " << fact << endl;
+        cout << "Factorial: " << fact << endl;
     }
 }
