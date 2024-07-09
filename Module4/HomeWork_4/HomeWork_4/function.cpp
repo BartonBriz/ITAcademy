a1351bccc520ee3fc17acc1ca020af83c1c74eb8
BrizGLy <Brizgly@mail.ru>
2024-07-09 18:41:33 +0300

    HW4 binar

---
+++ Module4/HomeWork_4/HomeWork_4/function.cpp
@@ -0,0 +1,18 @@
+#include "tools.h"
+void printone()
+{
+	COUT << "Chislo chetnoe i polojitelnoe:  ";
+}
+void printtwo()
+{
+	COUT << "Chislo chetnoe i otrichatelnoe : ";
+}
+void printthree()
+{
+	COUT << "Chislo ne chetnoe i otrichatelnoe: ";
+}
+void printfour()
+{
+	COUT << "Chislo ne chetnoe i polojitelnoe: ";
+}
+
---
+++ Module4/HomeWork_4/HomeWork_4/tools.h
@@ -0,0 +1,19 @@
+#pragma once
+#ifndef __Library__
+
+#include <iostream>
+#include <bitset>
+
+#endif // __Library__
+
+#ifndef __Func__
+#define BITSET std::bitset<8>
+#define Endl std::endl
+#define COUT std::cout
+#define CIN std::cin
+#endif // __Func__
+
+void printone();
+void printtwo();
+void printthree();
+void printfour();
