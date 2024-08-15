0db712a8ba545df9bfa7b9d2dc8f9b45827c3e09
Michael <Brizgly@mail.ru>
2024-08-15 21:34:57 +0300

    HW6 Smart

---
+++ Module6.2/hmYkazat/hmYkazat/tools1.cpp
@@ -0,0 +1,26 @@
+// shared_ptr
+
+
+#include "tools.h"
+
+void shared()
+{
+	int arr1{ 10 };
+	std::shared_ptr<int[]> s_ptr = std::make_shared<int[]>(arr1);
+	int n1;
+	int result{};
+	for (int i = 0; i < arr1; i++)
+	{
+		std::cout << "Vedite chislo: ";
+		std::cin >> n1;
+		s_ptr[i] = n1;
+	}
+	for (int i = 0; i < arr1; i++)
+	{
+		result += s_ptr[i];
+	}
+	std::cout << result << std::endl;
+
+
+
+}
---
+++ lesson_7_1/lesson_7_1/lesson_7_1.cpp
@@ -0,0 +1,32 @@
+﻿
+#include <iostream>
+#include <string>
+
+
+void print(std::string str) // str - parametr
+{
+	std::cout << str << std::endl;
+}
+int sum(int* a, int* b)
+{
+	//*a = 222;
+	return *a + *b;
+	
+}
+int sum1(int &a, int &b)
+{
+	return a - b;
+}
+
+int main()
+{
+	print("hello"); // hello - argyment
+
+	int value1 = 50;
+	int value2 = 50;
+	int result = sum(&value1, &value2);
+	int result1 = sum1(value1, value2);
+	std::cout << result << std::endl;
+	std::cout << result1 << std::endl;
+}
+
